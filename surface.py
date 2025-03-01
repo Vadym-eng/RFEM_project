efc02478c24dd29c632c0fae2b2e1a28d0f427b0
Vadym-eng <vyunkovskiyvadim@gmail.com>
2025-03-01 10:58:36 +0200

    Init

--- surface.py
+++ surface.py
@@ -7,6 +7,7 @@ from RFEM.TypesForSurfaces.surfaceStiffnessModification import *
 from RFEM.BasicObjects.surface import *
 from RFEM.BasicObjects.line import *
 from RFEM.BasicObjects.thickness import *
+from RFEM.Results.resultTables import *
 from RFEM.Loads.surfaceLoad import SurfaceLoad
 from decimal import Decimal as D
 import math as mt
@@ -14,15 +15,15 @@ import pandas as pd
 import numpy as np
 
 
-Model(new_model=False, model_name="Surface_1", delete=True)
+Model(new_model=False, model_name="Surface_2_unification", delete=True)
 Model.clientModel.service.begin_modification()
 LoadCase(1, "LC 1", [False], ActionCategoryType.ACTION_CATEGORY_PERMANENT_G)
 StaticAnalysisSettings(1, "LC 1", StaticAnalysisType.GEOMETRICALLY_LINEAR)
 
-x = 1  # длина поверхности (m)
-y = 1  # ширина поверхности
+x = D(1.5)  # длина поверхности (m)
+y = D(1.5)  # ширина поверхности
 len_wid_surfase = D(0.05)  # размер элемента сетки
-Ry = 0.235  # kN/mm^2
+Ry = 0.015  # kN/mm^2
 
 
 def equivalent_stress_in_surface(moment_mx, moment_my, moment_mxy, h):  # kN/cm^2
@@ -78,14 +79,32 @@ def required_thickness(moment_mx, moment_my, moment_mxy, Ry):  # mm
     )
 
 
-# mt.ceil(mt.sqrt(sect1[i - 1]) * 100) / 10000
+
+def hight_selection(stress, surf_num, surf_old_thikness):
+    global x, y, len_wid_surfase, Ry
+    if ((x + len_wid_surfase) // len_wid_surfase) * 2 < surf_num <= (
+        (x + len_wid_surfase) // len_wid_surfase
+    ) * ((y + len_wid_surfase) // len_wid_surfase) - (
+        (x + len_wid_surfase) // len_wid_surfase
+    ) * 2 or surf_num % (
+        (x + len_wid_surfase) // len_wid_surfase
+    ) not in [
+        1,
+        2,
+        0,
+        ((x + len_wid_surfase) // len_wid_surfase) - 1,
+    ]: # проверка на не входимость в зону поверхностей ребер
+        non_unificat_thickness = round((stress/Ry) * surf_old_thikness, 1) if round((stress/Ry) * surf_old_thikness, 1) >= 20.0 else 20.0
+        return min(math.ceil(non_unificat_thickness / 20) * 20, 200)
+    else:
+        return 200
 
 
 def creating_surface_geometry():
     global x, y, len_wid_surfase
     count_nodes = 1
     count_lines = 1
-    count_loads = []
+
 
     # цикл создания точек
     for i in np.arange(0, x + len_wid_surfase, len_wid_surfase):
@@ -147,16 +166,22 @@ def creating_surface_geometry():
             Thickness(
                 no=i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
                 name="Uniform | d : 200.0 mm | 2 - P235GH 1.0345",
-                material_no=2,
+                material_no=3,
                 uniform_thickness_d=0.2,
             )
 
-            Surface.Standard(
+            Surface.WithoutMemberaneTension(
                 i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
                 geometry_type=SurfaceGeometry.GEOMETRY_PLANE,
                 boundary_lines_no=f"{i + j*(int(((x) + len_wid_surfase) / len_wid_surfase))} {(i-1)*int((x + len_wid_surfase) // len_wid_surfase) + 2 + int((x + len_wid_surfase) // len_wid_surfase * (y + len_wid_surfase) // len_wid_surfase) + j} {(i)*int((x + len_wid_surfase) // len_wid_surfase) + 2 + int((x + len_wid_surfase) // len_wid_surfase * (y + len_wid_surfase) // len_wid_surfase) + j} {i + int((x + len_wid_surfase) // len_wid_surfase) + int((x + len_wid_surfase) // len_wid_surfase) * j}",
                 thickness=i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
             )
+            # Surface.Standard(
+            #     i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
+            #     geometry_type=SurfaceGeometry.GEOMETRY_PLANE,
+            #     boundary_lines_no=f"{i + j*(int(((x) + len_wid_surfase) / len_wid_surfase))} {(i-1)*int((x + len_wid_surfase) // len_wid_surfase) + 2 + int((x + len_wid_surfase) // len_wid_surfase * (y + len_wid_surfase) // len_wid_surfase) + j} {(i)*int((x + len_wid_surfase) // len_wid_surfase) + 2 + int((x + len_wid_surfase) // len_wid_surfase * (y + len_wid_surfase) // len_wid_surfase) + j} {i + int((x + len_wid_surfase) // len_wid_surfase) + int((x + len_wid_surfase) // len_wid_surfase) * j}",
+            #     thickness=i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
+            # )
 
     # цикл создания нагрузок на поверхности
     for j in range(0, int((x + len_wid_surfase) / len_wid_surfase)):
@@ -167,7 +192,7 @@ def creating_surface_geometry():
                 no=i + j * (int(((x) + len_wid_surfase) / len_wid_surfase)),
                 load_case_no=1,
                 surface_no=f"{i + j * (int(((x) + len_wid_surfase) / len_wid_surfase))}",
-                magnitude=500000.0,  # нагрузка 5 kg/cm^2 в n/m^2
+                magnitude=50000.0,  # нагрузка 0.5 kg/cm^2 в n/m^2
             )
             # SurfaceLoad(no=1, load_case_no=1, surface_no=f"{" ".join(count_loads)}", magnitude=5000.0)
 
@@ -200,6 +225,17 @@ def subsurface_calculation():
 
         if not mt.isnan(moment_mx):
 
+            equivalent_stresses = ResultTables.SurfacesEquivalentStressesMises(
+                loading_type=CaseObjectType.E_OBJECT_TYPE_LOAD_CASE,
+                loading_no=1,
+                object_no=i,
+            )
+            equivalent_stress_max = round(
+                equivalent_stresses[0]["equivalent_stresses_sigma_eqv_max"]
+                / 1000000000,
+                3,
+            )  # kN/mm^2
+
             surface_thickness_old = Surface.GetSurface(i).thickness  # mm
             surface_thickness_old_res = (
                 Thickness.GetThickness(
@@ -214,13 +250,13 @@ def subsurface_calculation():
             )  # заполнение списка значениями усилий (Mx, My, Mxy) на поверхности (kNm/m или kNmm/mm)
 
             stress.append(
-                equivalent_stress_in_surface(
-                    moment_mx, moment_my, moment_mxy, surface_thickness_old
-                )
+                round(equivalent_stress_max * 100, 1)
             )  #  заполнение списка экв. напряжениями (σ) (kN/cm^2)
 
+            surface_thickness_new = hight_selection(equivalent_stress_max, i, surface_thickness_old)  # mm
+
             thickness_new.append(
-                required_thickness(moment_mx, moment_my, moment_mxy, Ry)
+                surface_thickness_new
             )  # заполнение списка новой толщиной (h) (mm)
 
             conv_percent.append(
@@ -237,7 +273,7 @@ def subsurface_calculation():
             )
             thickness_old.append(surface_thickness_old_res), forces.append(
                 (moment_mx, moment_my, moment_mxy)
-            ), stress.append(0), thickness_new.append(0.1), conv_percent.append(
+            ), stress.append(Ry * 100), thickness_new.append(20.0), conv_percent.append(
                 round(abs(1 - (thickness_new[i - 1] / thickness_old[i - 1])), 2)
             )
 
@@ -245,8 +281,8 @@ def subsurface_calculation():
 
         Thickness(
             no=i,
-            name=f"Uniform | d : {thickness_new[i - 1]}.0 mm | 2 - P235GH 1.0345",
-            material_no=2,
+            name=f"Uniform | d : {thickness_new[i - 1]} mm | 2 - P235GH 1.0345",
+            material_no=3,
             uniform_thickness_d=thickness_new[i - 1] / 1000,
         )  # назначение результатов поверхности (h)
 
@@ -256,17 +292,12 @@ def subsurface_calculation():
 
 
 all_data = {}
-min_valie_checking_economic_cross_section = Ry - 0.01
+min_valie_checking_economic_cross_section = Ry * 100 - 1
 i = 1
 stress1, conv_percent1 = [1], [1]
-
+# min_valie_checking_economic_cross_section <= min(stress1) and
 while not (
-    (
-        min_valie_checking_economic_cross_section <= min(stress1)
-        and max(stress1) <= Ry
-        and max(conv_percent1) <= 0.05
-    )
-    or i == 41
+    (max(stress1) <= Ry * 100 and max(conv_percent1) <= 0.05) or i == 26
 ):  # проверка результатов итеррации и остановка процесса при при достижении одного из условий (1 - по результатам поверхностей, 2 - по достижению макс итераций)
     forces1, stress1, thickness_new1, conv_percent1 = subsurface_calculation()
 
@@ -294,7 +325,7 @@ results_columns = pd.DataFrame(
     all_data,
     index=[i for i in range(1, round(x / len_wid_surfase * y / len_wid_surfase) + 1)],
 )
-results_columns.to_excel("results_surface_1.xlsx")
+results_columns.to_excel("results_surface_5.xlsx")
 
 
 Model.clientModel.service.finish_modification()
